cf71f651381b8e4d06d38114f1d222a624d52a40
Skuttelw <stefan.kutt@gmx.at>
2020-12-08 22:00:42 +0100

    Projektdateien hinzufügen.

---
+++ Taschenrechner_2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Taschenrechner_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("gibt die Erste Zahl ein:");
+            string ersteZahl = Console.ReadLine(); 
+            Console.WriteLine("gibt die Zweite Zahl ein:");
+            string zweiteZahl = Console.ReadLine();
+
+            double erste = Convert.ToDouble(ersteZahl);
+            double zweite = Convert.ToDouble(zweiteZahl);
+
+
+            double erg = erste + zweite;
+            Console.WriteLine("das Ergebnis lautet {0}", erg);
+
+            Console.ReadLine();
+
+        }
+    }
+}
