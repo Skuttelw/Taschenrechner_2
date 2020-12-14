4aea2381fabc16ffff1ab7e350dc33a5695718b6
Skuttelw <stefan.kutt@gmx.at>
2020-12-14 23:31:30 +0100

    Methode Summieren und Benutzereingabe hinzugefügt

--- Taschenrechner_2/Program.cs
+++ Taschenrechner_2/Program.cs
@@ -6,27 +6,43 @@ namespace Taschenrechner_2
     {
         static void Main(string[] args)
         {
+            
+            //Eingabe der Zahlen
             Console.WriteLine("gibt die Erste Zahl ein:");
             string ersteZahl = Console.ReadLine(); 
             Console.WriteLine("gibt die Zweite Zahl ein:");
             string zweiteZahl = Console.ReadLine();
 
+
+            //Umwandeln in ein Double
             double erste = Convert.ToDouble(ersteZahl);
             double zweite = Convert.ToDouble(zweiteZahl);
 
-
-            double erg = erste + zweite;
+            double erg= Addiere(erste, zweite);
+            //Ausgabe
             Console.WriteLine("das Ergebnis lautet {0}", erg);
 
+
+            WartenaufBenutzereingabe();
+            
+            
+
+
+
+
+
+
+        }
+        static void WartenaufBenutzereingabe()
+        {
+            Console.WriteLine("Zum Beenden bitte Return drücken");
             Console.ReadLine();
-
-            
-            
-
-
-
-
-
+        }
+        static double Addiere(double ersteZahl, double zweiteZahl)
+        {
+            //Berechnung
+            double erg = ersteZahl + zweiteZahl;
+            return erg;
 
         }
     }
